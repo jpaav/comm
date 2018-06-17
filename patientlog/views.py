bd938fbff03dc44ad4535243381cb7d935112239
jpaav <joepaavola@gmail.com>
2018-06-17 18:55:11 -0400

    Added tag colors in the entry_fragment and scrolling for the fragments
    Log page no longer scrolls

--- patientlog/views.py
+++ patientlog/views.py
@@ -59,25 +59,40 @@ def log_detail(request, log_id, entry_id):
 		log = Log.objects.get(pk=log_id)
 	except ObjectDoesNotExist:
 		return render(request, 'patientlogs/object_does_not_exist.html', {'obj_type': 'log'})
-	# Properly filter entries depending on the contents of the url
+	# Get variables from URL that alter view
 	# Second argument is default value
-	filter = request.GET.get('filter', 'newest')
-	offset = int(request.GET.get('offset', '0'))
+	sort = request.GET.get('sort', 'newest')
+	try:
+		offset = int(request.GET.get('offset', '0'))
+	# If the user supplies some bogus offset it will be caught here and changed to zero
+	except ValueError:
+		return redirect('/logs/' + str(log_id) + '/?sort=' + str(sort) + '&offset=0')
 	count = Entry.objects.filter(log=log).count()
-	if count < 100:
-		offset = 0
-	if offset == -1:
-		offset = count - 100
-	if filter == 'newest':
+	# Returns the largest multiple of 100 less than count if the offset is too large or is -1
+	# The negative -1 option is so that the template can go to the end without knowing the count
+	if offset > count or offset == -1:
+		offset = int(math.floor(count / 100.0)) * 100
+		return redirect('/logs/' + str(log_id) + '/?sort=' + str(sort) + '&offset=' + str(offset))
+	# Returns the largest multiple of 100 less than offset
+	# Used to make sure offsets are multiples of 100 (ie 134 becomes 100 and 202 becomes 200)
+	if not offset % 100 == 0:
+		offset = int(math.floor(offset / 100.0)) * 100
+		return redirect('/logs/' + str(log_id) + '/?sort=' + str(sort) + '&offset=' + str(offset))
+	# Sorts by the newest timestamp in the entries
+	if sort == 'newest':
 		entries = Entry.objects.filter(log=log).order_by('-timestamp')[offset:100]
-	elif filter == 'oldest':
+	# Sorts by the oldest timestamp in the entries
+	elif sort == 'oldest':
 		entries = Entry.objects.filter(log=log).order_by('timestamp')[offset:100]
+	# If the user supplies some bogus filter it will be caught here and changed to newest
+	else:
+		return redirect('/logs/' + str(log_id) + '/?sort=newest&offset=' + str(offset))
 	# Get the detail object based on the entry_id
 	try:
 		detail = Entry.objects.get(pk=entry_id)
 	except ObjectDoesNotExist:
 		return render(request, 'patientlogs/object_does_not_exist.html', {'obj_type': 'entry'})
-	return render(request, 'patientlogs/log.html', {'log': log, 'entries': entries, 'detail': detail, 'filter': filter,
+	return render(request, 'patientlogs/log.html', {'log': log, 'entries': entries, 'detail': detail, 'sort': sort,
 													'offset': offset})
 
 
