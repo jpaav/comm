1ade8ec6e31c9ebf4abd4ce69201f828a2809e10
jpaav <joepaavola@gmail.com>
2018-06-25 09:57:51 -0400

    Fix for context processors?

--- comm/context_processors.py
+++ comm/context_processors.py
@@ -1,10 +1,13 @@
 from comm import settings
 from patientlog.models import Log
 
+import os
+
 
 def is_simple_processor(request):
 	return {'simple_ui': settings.SIMPLE_UI}
 
 
 def simple_log(request):
-	return {'simple_log': Log.objects.get(pk=settings.SIMPLE_LOG_ID)}
+	id = int(os.environ['SIMPLE_LOG_ID'])
+	return {'simple_log': Log.objects.get(pk=id)}
